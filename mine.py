b89afd65db8ed198e14676d1e1e5d226a593d45a
salami olanrewaju <slanxe15@gmail.com>
2023-10-22 13:28:40 +0100

    Fix critical bug: Update name and email in mine.py

--- mine.py
+++ mine.py
@@ -1,6 +1,6 @@
 # Define variables
-name = "PUT YOUR NAME"
-email = 'PUT YOUR EMAIL'
+name = "olanrewaju salami"
+email = 'slanxe15@gmail.com'
 
 # Print the name and email address
 print("Name:", name)
